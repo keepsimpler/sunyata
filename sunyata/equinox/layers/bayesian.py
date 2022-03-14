e1bdff87786e06d87c940643294bf8630e3f4156
fengwf <fengwenfeng@gmail.com>
2022-03-14 23:16:36 +0800

    map values to non negative, bayesian iteration

--- sunyata/equinox/layers/bayesian.py
+++ sunyata/equinox/layers/bayesian.py
@@ -1,26 +1,55 @@
+# %%
+import jax
 import jax.numpy as jnp
 import equinox as eqx
+from numpy import ndarray
 
-class BayesianLayer(eqx.Module):
-    positive_method: str = 'none'
-    
-    def __init__(self, positive_method:str = 'none'):
-        super().__init__()
-        self.positive_method = positive_method
+# %%
+class MapValuesToNonNegative(eqx.Module):
+    def __call__(self, x: jnp.ndarray):
+        pass
 
-    def __call__(self, priors, evidence):
-        if self.positive_method == 'abs':
-            evidence = jnp.abs(evidence)
-        elif self.positive_method == 'square':
-            evidence = evidence ** 2
-        elif self.positive_method == 'exp':
-            evidence = jnp.exp(evidence)
-            
-        weighted_sum_of_evidence = jnp.sum(priors * evidence, axis=-1, keepdims=True)
-        posteriors = (priors * evidence) / weighted_sum_of_evidence
+# %%
+class ByComputeAbsoluteValues(MapValuesToNonNegative):
+    """Map values to non negative by computing absolute values"""
+    def __call__(self, x: jnp.ndarray):
+        return jnp.abs(x)
+
+# %%
+class ByComputeSquaredValues(MapValuesToNonNegative):
+    def __call__(self, x: jnp.ndarray):
+        return x ** 2
+
+# %%
+class ByComputeExponentialValues(MapValuesToNonNegative):
+    def __call__(self, x: jnp.ndarray):
+        return jnp.exp(x)
+
+# %%
+class ByComputeReluAndSquaredValues(MapValuesToNonNegative):
+    def __call__(self, x: jnp.ndarray):
+        return jax.nn.relu(x) ** 2
+
+
+# %%
+class BayesianIteration(eqx.Module):
+    """One iteration of Bayes' theorem, get posteriors from priors and evidences"""
+    def __call__(self, priors: jnp.ndarray, evidences: jnp.ndarray) -> jnp.ndarray:
+        total_evidence = jnp.sum(priors * evidences, axis=-1, keepdims=True)
+        posteriors = (priors * evidences) / total_evidence
         
         return posteriors
 
-
+# %%
 if __name__ == '__main__':
-    bayesian_layer = BayesianLayer(positive_method='abs')
+    priors = jnp.ones(6) / 6
+    x = jnp.array([-2, -1, 0, 1, 2, 3])
+
+    map_to_non_negative_by_compute_absolute_values = ByComputeAbsoluteValues()
+    evidences = map_to_non_negative_by_compute_absolute_values(x)
+    print(evidences)
+
+    bayesian_iteration = BayesianIteration()
+    posteriors = bayesian_iteration(priors, evidences)
+    print(posteriors)
+# %%
