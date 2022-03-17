d2951ee299261339c09c78b56d203d6733003715
fengwf <fengwenfeng@gmail.com>
2022-03-17 09:15:31 +0800

    get all files with specific filetypes in directory

--- sunyata/utils.py
+++ sunyata/utils.py
@@ -1,7 +1,16 @@
+import glob
+from typing import List
 import jax.numpy as jnp
 from jax import lax
 
 import numpy as np
+
+def get_all_files_with_specific_filetypes_in_a_directory(directory: str, filetypes: List[str]=["*"]):
+    files_with_specific_filetypes = []
+    for filetype in filetypes:
+        files_with_specific_filetypes.extend(glob.glob(directory + f"*.{filetype}"))
+        
+    return files_with_specific_filetypes
 
 def smoothing_func(batch: np.ndarray, vocab_size: int, smoothing_ratio: float):
     x = np.ones(batch.shape + (vocab_size,)) * smoothing_ratio / vocab_size
@@ -15,7 +24,7 @@ def smoothing_func(batch: np.ndarray, vocab_size: int, smoothing_ratio: float):
 
 
 # copy from flax.training.common_utils.onehot
-def onehot(seq, vocab_size, on_value=1.0, off_value=0.0):
+def one_hot(seq, vocab_size, on_value=1.0, off_value=0.0):
     x = (seq[..., None] == jnp.arange(vocab_size).reshape((1,) * seq.ndim + (-1,)))
     x = lax.select(x, jnp.full(x.shape, on_value), jnp.full(x.shape, off_value))
     return x.astype(jnp.float32)
