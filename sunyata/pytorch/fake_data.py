2b96be799543d732ed5eab69e3058b10df10b60e
feng wenfeng <fengwenfeng@gmail.com>
2022-03-23 08:45:46 +0800

    fake lm data

---
+++ sunyata/pytorch/fake_data.py
@@ -0,0 +1,28 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+import pytorch_lightning as pl
+
+
+class FakeLMDataset(Dataset):
+    def __init__(self, sequences_num: int, vocab_size: int, seq_len: int):
+        torch.manual_seed(1)
+        self.data = torch.randint(0, vocab_size-1, (sequences_num, seq_len+1))
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        input = self.data[index, :-1]
+        target = self.data[index, 1:]
+        return input, target
+
+
+def yield_fake_data(batch_size, seq_len, vocab_size, batchs_num):
+    torch.manual_seed(1)
+    for _ in range(batchs_num):
+        batch = torch.randint(0, vocab_size-1, (batch_size, seq_len+1))
+        input = batch[:, :-1]
+        target = batch[:, 1:]
+        yield input, target
+
---
+++ sunyata/tests/pytorch/test_fake_data.py
@@ -0,0 +1,13 @@
+from sunyata.pytorch.fake_data import yield_fake_data
+
+
+def test_yield_fake_data():
+    batch_size, seq_len, vocab_size, batchs_num = 2, 8, 16, 8
+    fake_data = yield_fake_data(batch_size, seq_len, vocab_size, batchs_num)
+    for i, (input, target) in enumerate(fake_data):
+        pass
+    
+    assert i == batchs_num - 1
+    assert input.shape == (batch_size, seq_len)
+    assert target.shape == (batch_size, seq_len)
+
