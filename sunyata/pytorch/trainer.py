f0a2b9c630742e837fb153d5d075d6f37cc911d0
feng wenfeng <fengwenfeng@gmail.com>
2022-03-23 16:27:27 +0800

    minor

--- sunyata/pytorch/trainer.py
+++ sunyata/pytorch/trainer.py
@@ -4,32 +4,34 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from sunyata.pytorch.hidden_bayesian_net import HiddenBayesianNet
 from sunyata.pytorch.transformer import TransformerLayer
-from sunyata.pytorch.fake_data import FakeLMDataset
+from sunyata.pytorch.wikitext import WikiText2DataModule
 
 import pytorch_lightning as pl
 # %%
-trainer = pl.Trainer(gpus=1)
+batch_size = 32
+seq_len = 32
+wikitext2_data_module = WikiText2DataModule(batch_size, seq_len + 1)
 # %%
-vocab_size=2**8
-fake_lm_dataset = FakeLMDataset(sequences_num=2**12, vocab_size=vocab_size, seq_len=32)
+hidden_dim = 200
+expanded_dim = 1 * hidden_dim
+num_heads = 2
+dropout = 0.2
+transformer = TransformerLayer(hidden_dim, expanded_dim, num_heads, dropout)
 # %%
-train_dataloader = DataLoader(fake_lm_dataset, batch_size=8)
+num_layers = 8
+# layers = [TransformerLayer(hidden_dim, expanded_dim, num_heads, dropout).cuda() for _ in range(num_layers)]
+layers = [nn.Linear(hidden_dim, hidden_dim).cuda() for _ in range(num_layers)]
 # %%
-hidden_dim = 16
-expanded_dim = 2 * hidden_dim
-num_heads = 8
-dropout = 0.1
-transformer = TransformerLayer(embed_dim=hidden_dim, hidden_dim=expanded_dim, num_heads=num_heads, dropout=dropout)
-# %%
-num_layers = 2
-layers = [TransformerLayer(embed_dim=hidden_dim, hidden_dim=expanded_dim, num_heads=num_heads, dropout=dropout).cuda() for _ in range(num_layers)]
-
 layers = nn.Sequential(*layers)
+layers = [layers]
 # %%
 layers = [nn.Identity()]
 # %%
+vocab_size = wikitext2_data_module.vocab_size
 learning_rate = 1e-3
-hidden_bayesian_net = HiddenBayesianNet(layers, vocab_size, hidden_dim, learning_rate)
+hidden_bayesian_net = HiddenBayesianNet(layers, vocab_size, hidden_dim, learning_rate, sharing_weight=True)
 # %%
-trainer.fit(hidden_bayesian_net, train_dataloader)
+trainer = pl.Trainer(gpus=1)
+# %%
+trainer.fit(hidden_bayesian_net, wikitext2_data_module)
 # %%
