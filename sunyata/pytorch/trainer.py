3ba921a57943bdd55afbc4b235740333e256d7d9
feng wenfeng <fengwenfeng@gmail.com>
2022-03-25 08:09:58 +0800

    minor

--- sunyata/pytorch/trainer.py
+++ sunyata/pytorch/trainer.py
@@ -1,6 +1,7 @@
 # %%
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from sunyata.pytorch.hidden_bayesian_net import HiddenBayesianNet
 from sunyata.pytorch.transformer import TransformerLayer
@@ -18,7 +19,7 @@ num_heads = 2
 dropout = 0.2
 transformer = TransformerLayer(hidden_dim, expanded_dim, num_heads, dropout)
 # %%
-num_layers = 4
+num_layers = 2
 # layers = [TransformerLayer(hidden_dim, expanded_dim, num_heads, dropout).cuda() for _ in range(num_layers)]
 layers = [nn.Linear(hidden_dim, hidden_dim).cuda() for _ in range(num_layers)]
 # %%
@@ -27,9 +28,15 @@ layers = [nn.Linear(hidden_dim, hidden_dim).cuda() for _ in range(num_layers)]
 # %%
 # layers = [nn.Identity()]
 # %%
+def relu_and_square(x: torch.Tensor):
+  return F.relu(x) ** 2
+
+def square(x: torch.Tensor):
+  return x ** 2
+# %%
 vocab_size = wikitext2_data_module.vocab_size
-learning_rate = 1e-3
-hidden_bayesian_net = HiddenBayesianNet(layers, vocab_size, hidden_dim, learning_rate, sharing_weight=False)
+learning_rate = 1e-1
+hidden_bayesian_net = HiddenBayesianNet(layers, vocab_size, hidden_dim, learning_rate, to_non_negative=relu_and_square, sharing_weight=False)
 # %%
 trainer = pl.Trainer(gpus=1)
 # %%
