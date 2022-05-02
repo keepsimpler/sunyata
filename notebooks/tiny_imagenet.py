26e16721856a3fbba52312cdfd071bdc23fe950a
feng wenfeng <fengwenfeng@gmail.com>
2022-05-02 22:26:19 +0800

    add hyperparameters

--- notebooks/tiny_imagenet.py
+++ notebooks/tiny_imagenet.py
@@ -1,67 +1,63 @@
 # %%
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
 from sunyata.pytorch.tiny_imagenet import TinyImageNet, TinyImageNetDataModule
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
+from sunyata.pytorch.bayes.vision import DeepBayesInferVision, DeepBayesInferVisionCfg
+from sunyata.pytorch.layers.transformer import TransformerLayer
+# %%
+cfg = DeepBayesInferVisionCfg(
+    image_size = 64,
+    patch_size = 8,
+    num_classes = 200,
+    hidden_dim= 128,  #  1024
+    num_heads = 2,  # 16
+    expanded_dim = 256,  # 2048
+#     is_mask = False,
+    pool = 'cls',
+    channels = 3,
+#     dim_head = 64,
+#     dropout = 0.1,
+    emb_dropout = 0.,
+    num_layers = 8,
+    batch_size = 16, 
+    learning_rate = 1e-3)
+
 # %%
 train_transforms = transforms.Compose(
     [
-        # transforms.Resize(224),
-        # transforms.RandomResizedCrop(224),
-        # transforms.RandomHorizontalFlip(),
+#         transforms.Resize(224),
+#         transforms.RandomResizedCrop(224),
+#         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
     ]
 )
 
+tiny_image_net_datamodule = TinyImageNetDataModule(
+    batch_size=cfg.batch_size, root=".data/",
+    train_transforms=train_transforms, 
+    val_transforms=train_transforms)
 # %%
-train_data = TinyImageNet(root='.data', split='train', transform=train_transforms)
-
-image, target = train_data[0]
-image.shape
-
-# %%
-batch_size = 2
-train_loader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True)
-
-len(train_data), len(train_loader)
-# %%
-image, target = next(iter(train_loader))
-image.shape, target.shape
-
-
-# %%
-import torch
-from torch import nn
-
-from sunyata.pytorch.bayes.vision import DeepBayesInferVision, DeepBayesInferVisionCfg
-from sunyata.pytorch.layers.transformer import TransformerLayer
+from pytorch_lightning import loggers as pl_loggers
+csv_logger = pl_loggers.CSVLogger(save_dir="lightning_logs/", 
+    name="tiny-imagenet", version=1)
+trainer = pl.Trainer(gpus=1, 
+                     max_epochs=1, 
+                     limit_train_batches=100,  # 1.0 
+                     limit_val_batches=10,  # 1.0 
+                     log_every_n_steps=10,
+                     logger=csv_logger)
 
 # %%
-cfg = DeepBayesInferVisionCfg(
-    image_size = 224,
-    patch_size = 16,
-    num_classes = 200,
-    hidden_dim= 1024,
-    num_layers= 6,
-    num_heads = 16,
-    expanded_dim = 2048,
-    pool = 'cls',
-    channels = 3,
-    dim_head = 64,
-    dropout = 0.1,
-    emb_dropout = 0.1,
-    batch_size=2, 
-    learning_rate=1e-3)
-
-# %%
-transformer = TransformerLayer(cfg)
-layers = [TransformerLayer(cfg) for _ in range(2)]
-
-# %%
+layers = [TransformerLayer(cfg) for _ in range(cfg.num_layers)]
 deep_bayes_net = DeepBayesInferVision(layers, cfg)
-
-log_posterior = deep_bayes_net(image)
-posterior = torch.exp(log_posterior)
-
-assert torch.allclose(posterior.sum(dim=-1), torch.tensor(1.)) 
-
+trainer.fit(deep_bayes_net, tiny_image_net_datamodule)
+# %%
+layers = [TransformerLayer(cfg) for _ in range(cfg.num_layers)]
+layers = [nn.Sequential(*layers)]
+deep_bayes_net = DeepBayesInferVision(layers, cfg)
+trainer.fit(deep_bayes_net, tiny_image_net_datamodule)
+# %%
