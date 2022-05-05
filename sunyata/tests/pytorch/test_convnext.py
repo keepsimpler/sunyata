448d70f0666c1478aca310b0e29bb59c1593e69f
feng wenfeng <fengwenfeng@gmail.com>
2022-05-05 17:46:09 +0800

    ResNeXt-ify

--- sunyata/tests/pytorch/test_convnext.py
+++ sunyata/tests/pytorch/test_convnext.py
@@ -3,17 +3,16 @@ import torch
 
 from sunyata.pytorch.convnext import BottleNeckBlock, ConvNextStage, ConvNextStem, ConvNextEncoder
 
-x = torch.rand(1, 32, 7, 7)
-block = BottleNeckBlock(32, 64)
-block(x).shape
+image = torch.rand(1, 3, 224, 224)
+block = BottleNeckBlock(3, 64)
+block(image).shape
 # %%
-stage = ConvNextStage(32, 64, depth=2)
-stage(x).shape
+stage = ConvNextStage(3, 64, depth=2)
+stage(image).shape
 # %%
 stem = ConvNextStem(3, 64)
-stem(x).shape
+stem(image).shape
 # %%
-image = torch.rand(1, 3, 224, 224)
 encoder = ConvNextEncoder(in_channels=3, stem_features=64, depths=[3,3,9,3], widths=[256, 512, 1024, 2048])
 encoder(image).shape
 # %%
