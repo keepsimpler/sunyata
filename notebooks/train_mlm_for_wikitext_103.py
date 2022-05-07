39590ed28d3b238d451383d2327e4175f2a05ba7
feng wenfeng <fengwenfeng@gmail.com>
2022-05-07 17:53:13 +0800

    refactor filenames

---
+++ notebooks/train_mlm_for_wikitext_103.py
@@ -0,0 +1,68 @@
+# %%
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+from pytorch_lightning import loggers as pl_loggers
+
+from sunyata.pytorch.wikitext103 import WikiText103DataModule
+
+from sunyata.pytorch.bayes import DeepBayesInferMLM, DeepBayesInferMLMCfg
+from sunyata.pytorch.layers.transformer import TransformerLayer
+
+# %%
+cfg = DeepBayesInferMLMCfg(
+    vocab_size = 20000,
+    hidden_dim = 64,
+    num_heads = 2,
+    expanded_dim = 128,
+    seq_len = 128,
+    batch_size = 8,
+    learning_rate = 1e-3,
+)
+# %%
+data_dir = ".data/wikitext-103-v1/"
+batch_size = cfg.batch_size
+vocab_size = cfg.vocab_size
+seq_len = cfg.seq_len
+
+wikitext103_datamodule = WikiText103DataModule(data_dir, batch_size, vocab_size, seq_len)
+# %%
+csv_logger = pl_loggers.CSVLogger(save_dir="lightning_logs/", 
+    name="wikitext_103", version=1)
+trainer = pl.Trainer(gpus=1, 
+                     max_epochs=1, 
+                     limit_train_batches=100,  # 1.0 
+                     limit_val_batches=10,  # 1.0 
+                     log_every_n_steps=10,
+                     logger=csv_logger)
+
+# %%
+layers = [TransformerLayer(cfg) for _ in range(cfg.num_layers)]
+deep_bayes_net = DeepBayesInferLM(layers, cfg)
+# %%
+input, target = next(iter(wikitext103_datamodule.train_dataloader()))
+
+# %%
+trainer.fit(deep_bayes_net, wikitext103_datamodule)
+
+
+# %%
+csv_logger = pl_loggers.CSVLogger(save_dir="lightning_logs/", 
+    name="wikitext_103", version=1)
+trainer = pl.Trainer(gpus=1, 
+                     max_epochs=1, 
+                     limit_train_batches=100,  # 1.0 
+                     limit_val_batches=10,  # 1.0 
+                     log_every_n_steps=10,
+                     logger=csv_logger)
+
+# %%
+layers = [TransformerLayer(cfg) for _ in range(cfg.num_layers)]
+layers = [nn.Sequential(*layers)]
+deep_bayes_net = DeepBayesInferLM(layers, cfg)
+trainer.fit(deep_bayes_net, wikitext103_datamodule)
+# %%
+output = deep_bayes_net(input)
+# %%
+output.shape
+# %%
