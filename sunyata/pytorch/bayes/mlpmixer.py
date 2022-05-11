eadf105928cc0e90cc6a61dd1426b9a6b6c458f7
feng wenfeng <fengwenfeng@gmail.com>
2022-05-11 21:04:45 +0800

    refactor

--- sunyata/pytorch/bayes/mlpmixer.py
+++ sunyata/pytorch/bayes/mlpmixer.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 from functools import partial
-from typing import List
 import pytorch_lightning as pl
 import torch
 import torch.nn as nn
@@ -28,8 +27,11 @@ class DeepBayesInferMlpMixerCfg:
     is_bayes: bool = True
     is_prior_as_params: bool =False
 
-    num_epochs: int = 3
+    num_epochs: int = 10
     learning_rate: float = 1e-3
+    optimizer_method: str = "Adam"  # or "AdamW"
+    learning_rate_scheduler: str = "CosineAnnealing"
+    weight_decay: float = None  # of "AdamW"
 
 
 
@@ -41,16 +43,20 @@ class DeepBayesInferMlpMixer(pl.LightningModule):
         assert (image_h % cfg.patch_size) == 0 and (image_w % cfg.patch_size) == 0, 'image must be divisible by patch size'
         num_patches = (image_h // cfg.patch_size) * (image_w // cfg.patch_size)
         chan_first, chan_last = partial(nn.Conv1d, kernel_size = 1), nn.Linear
-        self.layers = nn.Sequential(
-            *[nn.Sequential(
+        self.layers = nn.ModuleList([
+            nn.Sequential(
                 PreNormResidual(cfg.hidden_dim, FeedForward(num_patches, cfg.expansion_factor, cfg.dropout, chan_first)),
                 PreNormResidual(cfg.hidden_dim, FeedForward(cfg.hidden_dim, cfg.expansion_factor_token, cfg.dropout, chan_last))
-            ) for _ in range(cfg.num_layers)]
-        )
+            ) for _ in range(cfg.num_layers)
+        ])
+        if not cfg.is_bayes:
+            self.layers = nn.ModuleList([nn.Sequential(*self.layers)])  # to one layer
+
         self.embed = nn.Sequential(
             Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=cfg.patch_size, p2=cfg.patch_size),
             nn.Linear((cfg.patch_size ** 2) * cfg.channels, cfg.hidden_dim)
         )
+
         self.digup = nn.Sequential(
             nn.LayerNorm(cfg.hidden_dim),
             Reduce('b n c -> b c', 'mean'),
@@ -65,48 +71,54 @@ class DeepBayesInferMlpMixer(pl.LightningModule):
         else:
             self.register_buffer('log_prior', log_prior) 
 
-        self.learning_rate = cfg.learning_rate  
+        self.cfg = cfg  
     
     def forward(self, x):
         batch_size, _, _, _ = x.shape
         log_prior = repeat(self.log_prior, '1 n -> b n', b=batch_size)
 
         x = self.embed(x)
-        if self.is_bayes:
-            for layer in self.layers:
-                x = layer(x)
-                logits = self.digup(x)
-                log_posterior = log_bayesian_iteration(log_prior, logits)
-                log_prior = log_posterior
-        else:
-            x = self.layers(x)
+        for layer in self.layers:
+            x = layer(x)
             logits = self.digup(x)
             log_posterior = log_bayesian_iteration(log_prior, logits)
+            log_prior = log_posterior
 
         return log_posterior
 
-    def training_step(self, batch, batch_idx):
+    def _step(self, batch, mode="train"):  # or "val"
         input, target = batch
         log_posterior = self.forward(input)
         loss = F.nll_loss(log_posterior, target)
-        self.log("train_loss", loss)
+        self.log(mode + "_loss", loss)
         accuracy = (log_posterior.argmax(dim=-1) == target).float().mean()
-        self.log("train_accuracy", accuracy)
+        self.log(mode + "_accuracy", accuracy)
         return loss
 
+    def training_step(self, batch, batch_idx):
+        return self._step(batch, mode="train")
+
     def validation_step(self, batch, batch_idx):
-        input, target = batch
-        log_posterior = self.forward(input)
-        loss = F.nll_loss(log_posterior, target)
-        self.log("val_loss", loss)
-        accuracy = (log_posterior.argmax(dim=-1) == target).float().mean()
-        self.log("val_accuracy", accuracy)
-        # return loss
+        return self._step(batch, mode="val")
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
-        return optimizer
+        if self.cfg.optimizer_method == "Adam":
+            optimizer = torch.optim.Adam(self.parameters(), lr=self.cfg.learning_rate)
+        elif self.cfg.optimizer_method == "AdamW":
+            optimizer = torch.optim.AdamW(self.parameters(), lr=self.cfg.learning_rate, weight_decay=self.cfg.weight_decay)
+        else:
+            raise Exception("Only support Adam and AdamW optimizer till now.")
 
+        if self.cfg.learning_rate_scheduler == "CosineAnnealing":
+            lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, self.cfg.num_epochs)
+        else:
+            lr_scheduler = None
+
+        if lr_scheduler is None:
+            return optimizer
+        else:
+            return [optimizer], [lr_scheduler]
+        
 
 
 class PreNormResidual(nn.Module):
