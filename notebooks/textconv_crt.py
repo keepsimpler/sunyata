34b95c6cbd6b8272fab71aa3ac364f068c8d7077
feng wenfeng <fengwenfeng@gmail.com>
2022-05-24 06:51:08 +0800

    chinese remainder theorem version

---
+++ notebooks/textconv_crt.py
@@ -0,0 +1,64 @@
+# %%
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import pytorch_lightning as pl
+
+# %%
+from sunyata.pytorch.data.wikitext import (WikiTextDataModule,
+                                            shift_one_token,
+                                            split_to_two_parts)
+from sunyata.pytorch.arch.textconv_crt import TextConvCrt, TextConvCrtCfg
+
+# %%
+cfg = TextConvCrtCfg(
+    r1 = 122,
+    r2 = 121,
+    expansion = 2,
+    vocab_size = 10000,
+    seq_len = 128,
+    batch_size = 16,
+    kernel_size = 3,
+    num_layers = 12,
+
+    num_epochs = 10,
+    learning_rate = 1e-3
+)
+
+# %%
+wikitext2 = WikiTextDataModule(subset="2", 
+                   data_dir=".data/wikitext/", 
+                   batch_size=cfg.batch_size,
+                   vocab_size=cfg.vocab_size,
+                   seq_len=cfg.seq_len,
+                   collate_fn=split_to_two_parts)  # shift_one_token  None
+# %%
+wikitext2.tokenizer.decode(wikitext2.train_data[0].tolist(), skip_special_tokens=False)
+# https://colab.research.google.com/github/huggingface/notebooks/blob/master/examples/tokenizer_training.ipynb
+# https://www.thepythoncode.com/article/pretraining-bert-huggingface-transformers-in-python
+
+# %%
+(input_r1, input_r2), (target_r1, target_r2) = next(iter(wikitext2.train_dataloader()))
+input_r1.shape, target_r1.shape
+
+# %%
+textconvcrt = TextConvCrt(cfg)
+textconvcrt.summarize(max_depth=2)
+
+# %%
+csv_logger = pl.loggers.CSVLogger(save_dir="lightning_logs/", 
+    name="wikitext_2") # , version=2
+trainer = pl.Trainer(gpus=1, 
+                     max_epochs=cfg.num_epochs, 
+                     enable_checkpointing=False,
+                    #  limit_train_batches=100,  # 1.0 
+                    #  limit_val_batches=10,  # 1.0 
+                     log_every_n_steps=50,
+                     logger=csv_logger)
+
+# %%
+trainer.fit(textconvcrt, wikitext2)
+
+
+# %%
---
+++ sunyata/pytorch/arch/textconv_crt.py
@@ -0,0 +1,138 @@
+from dataclasses import dataclass
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+@dataclass
+class TextConvCrtCfg:                                            
+    r1: int = 122
+    r2: int = 121
+    expansion: int = 1
+    hidden_dim: int = (r1 + r2) * expansion
+
+    vocab_size: int = 1000
+    seq_len: int = 128
+    batch_size: int = 16
+
+    kernel_size: int = 3
+
+    num_layers: int = 6
+
+    num_epochs: int = 10
+    learning_rate: float = 1e-3
+    weight_decay: float = None
+    optimizer_method: str = "Adam"
+    learning_rate_scheduler: str = "CosineAnnealing"
+
+
+class TextConvCrt(pl.LightningModule):
+    def __init__(self, cfg: TextConvCrtCfg):
+        super().__init__()
+        self.save_hyperparameters("cfg")
+
+        cfg.hidden_dim = (cfg.r1 + cfg.r2) * cfg.expansion
+        self.embed_r1 = nn.Embedding(cfg.r1, cfg.r1 * cfg.expansion)
+        self.embed_r2 = nn.Embedding(cfg.r2, cfg.r2 * cfg.expansion)
+        self.digup_r1 = nn.Linear(cfg.hidden_dim, cfg.r1)
+        self.digup_r2 = nn.Linear(cfg.hidden_dim, cfg.r2)
+
+        self.layers = nn.Sequential(*[
+            nn.Sequential(
+                Residual(nn.Sequential(
+                    Conv1dThenLeftPad(cfg.hidden_dim, cfg.kernel_size),
+                    nn.GELU(),
+                    nn.BatchNorm1d(cfg.hidden_dim)  # LayerNorm1d nn.GroupNorm(1, cfg.hidden_dim) nn.InstanceNorm1d(cfg.hidden_dim, affine=True)
+                )),
+                nn.Conv1d(cfg.hidden_dim, cfg.hidden_dim, kernel_size=1),
+                nn.GELU(),
+                nn.BatchNorm1d(cfg.hidden_dim)
+            ) for _ in range(cfg.num_layers)
+        ])
+
+        self.cfg = cfg
+
+    def forward(self, x_r1, x_r2):
+        x_r1 = self.embed_r1(x_r1)
+        x_r2 = self.embed_r2(x_r2)
+        x = torch.cat((x_r1, x_r2), dim=-1)
+        x = x.permute(0, 2, 1)
+        x1 = x
+        for layer in self.layers:
+            x1 = layer(x1)
+            x = x + x1
+        # x = self.layers(x)
+        x = x.permute(0, 2, 1)
+        x_r1 = self.digup_r1(x)  #  x[:, :, :self.cfg.r1]
+        x_r2 = self.digup_r2(x)  # x[:, :, self.cfg.r1:]
+        return x_r1, x_r2
+
+    def _step(self, batch, mode="train"):  # or "val"
+        (input_r1, input_r2), (target_r1, target_r2) = batch
+        logits_r1, logits_r2 = self.forward(input_r1, input_r2)
+        logits_r1 = logits_r1.permute(0, 2, 1)
+        loss_r1 = F.cross_entropy(logits_r1, target_r1)
+        logits_r2 = logits_r2.permute(0, 2, 1)
+        loss_r2 = F.cross_entropy(logits_r2, target_r2)
+        loss = (loss_r1 + loss_r2) / 2
+        self.log(mode + "_loss", loss)
+        accuracy = torch.logical_and(logits_r1.argmax(dim=1) == target_r1, logits_r2.argmax(dim=1) == target_r2).float().mean()
+        self.log(mode + "_accuracy", accuracy)
+        return loss
+
+    def training_step(self, batch, batch_idx):
+        return self._step(batch, mode="train")
+
+    def validation_step(self, batch, batch_idx):
+        return self._step(batch, mode="val")
+
+    def configure_optimizers(self):
+        if self.cfg.optimizer_method == "Adam":
+            optimizer = torch.optim.Adam(self.parameters(), lr=self.cfg.learning_rate)
+        elif self.cfg.optimizer_method == "AdamW":
+            optimizer = torch.optim.AdamW(self.parameters(), lr=self.cfg.learning_rate, weight_decay=self.cfg.weight_decay)
+        else:
+            raise Exception("Only support Adam and AdamW optimizer till now.")
+
+        if self.cfg.learning_rate_scheduler == "CosineAnnealing":
+            lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, self.cfg.num_epochs)
+        else:
+            lr_scheduler = None
+
+        if lr_scheduler is None:
+            return optimizer
+        else:
+            return [optimizer], [lr_scheduler]   
+
+
+class LayerNorm1d(nn.Module):
+    def __init__(self, hidden_dim: int):
+        super().__init__()
+        self.layer_norm = nn.LayerNorm(hidden_dim)
+
+    def forward(self, x):
+        x = x.permute(0, 2, 1)
+        x = self.layer_norm(x)
+        x = x.permute(0, 2, 1)
+        return x
+
+
+class Conv1dThenLeftPad(nn.Module):
+    def __init__(self, hidden_dim: int, kernel_size: int):
+        super().__init__()
+        self.conv1d = nn.Conv1d(hidden_dim, hidden_dim, kernel_size, groups=hidden_dim)
+        self.kernel_size = kernel_size
+
+    def forward(self, x):
+        return self.conv1d(F.pad(x, (self.kernel_size - 1, 0)))
+
+
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x):
+        return self.fn(x) # + x
+
