dbc8472a74cdab38d1339e6d264259a6f5bc29f3
feng wenfeng <fengwenfeng@gmail.com>
2022-05-26 08:19:52 +0800

    base config

---
+++ sunyata/pytorch/arch/base.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class BaseCfg:
+    batch_size: int = 16
+
+    num_layers: int = 8
+    
+    num_epochs: int = 10
+    learning_rate: float = 1e-3
+    weight_decay: float = None
+    optimizer_method: str = "Adam"
+    learning_rate_scheduler: str = "CosineAnnealing"
