5c0adcea429ae894598bf9ed83537ecc1e9e69ae
feng wenfeng <fengwenfeng@gmail.com>
2022-05-31 15:39:12 +0800

    pl_bolts CIFAR10DataModule

--- notebooks/cifar10.py
+++ notebooks/cifar10.py
@@ -7,20 +7,17 @@ import torch.nn.functional as F
 import torchvision
 from pl_bolts.datamodules import CIFAR10DataModule
 from pl_bolts.transforms.dataset_normalizations import cifar10_normalization
-from pytorch_lightning import LightningModule, Trainer, seed_everything
+
+import pytorch_lightning as pl
 from pytorch_lightning.callbacks import LearningRateMonitor
-from pytorch_lightning.loggers import TensorBoardLogger
-from torch.optim.lr_scheduler import OneCycleLR
-from torch.optim.swa_utils import AveragedModel, update_bn
-from torchmetrics.functional import accuracy
+from pytorch_lightning.loggers import CSVLogger
 # %%
 
-seed_everything(7)
+pl.seed_everything(7)
 
-PATH_DATASETS = os.environ.get("PATH_DATASETS", "./.data")
-AVAIL_GPUS = min(1, torch.cuda.device_count())
-BATCH_SIZE = 256 if AVAIL_GPUS else 64
-NUM_WORKERS = int(os.cpu_count() / 2)
+PATH_DATASETS = os.environ.get("PATH_DATASETS", ".data")
+BATCH_SIZE = 32
+NUM_WORKERS = 0
 
 # %%
 train_transforms = torchvision.transforms.Compose(
@@ -48,77 +45,37 @@ cifar10_dm = CIFAR10DataModule(
     val_transforms=test_transforms,
 )
 # %%
-def create_model():
-    model = torchvision.models.resnet18(pretrained=False, num_classes=10)
-    model.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
-    model.maxpool = nn.Identity()
-    return model
+from sunyata.pytorch.arch.convmixer import DeepBayesInferConvMixer, DeepBayesInferConvMixerCfg
+
+cfg = DeepBayesInferConvMixerCfg(
+    hidden_dim = 256,
+    num_layers = 8,
+    kernel_size = 5,
+    patch_size = 2,
+    num_classes = 10,
+
+    is_bayes = True,
+    is_prior_as_params = False,
+
+    num_epochs = 1,
+    learning_rate = 1e-3,
+    optimizer_method = "Adam",  # or "AdamW"
+    learning_rate_scheduler = "CosineAnnealing",
+    weight_decay = None,  # of "AdamW"
+)
 # %%
-class LitResnet(LightningModule):
-    def __init__(self, lr=0.05):
-        super().__init__()
+model = DeepBayesInferConvMixer(cfg)
 
-        self.save_hyperparameters()
-        self.model = create_model()
-
-    def forward(self, x):
-        out = self.model(x)
-        return F.log_softmax(out, dim=1)
-
-    def training_step(self, batch, batch_idx):
-        x, y = batch
-        logits = self(x)
-        loss = F.nll_loss(logits, y)
-        self.log("train_loss", loss)
-        return loss
-
-    def evaluate(self, batch, stage=None):
-        x, y = batch
-        logits = self(x)
-        loss = F.nll_loss(logits, y)
-        preds = torch.argmax(logits, dim=1)
-        acc = accuracy(preds, y)
-
-        if stage:
-            self.log(f"{stage}_loss", loss, prog_bar=True)
-            self.log(f"{stage}_acc", acc, prog_bar=True)
-
-    def validation_step(self, batch, batch_idx):
-        self.evaluate(batch, "val")
-
-    def test_step(self, batch, batch_idx):
-        self.evaluate(batch, "test")
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.SGD(
-            self.parameters(),
-            lr=self.hparams.lr,
-            momentum=0.9,
-            weight_decay=5e-4,
-        )
-        steps_per_epoch = 45000 // BATCH_SIZE
-        scheduler_dict = {
-            "scheduler": OneCycleLR(
-                optimizer,
-                0.1,
-                epochs=self.trainer.max_epochs,
-                steps_per_epoch=steps_per_epoch,
-            ),
-            "interval": "step",
-        }
-        return {"optimizer": optimizer, "lr_scheduler": scheduler_dict}
-# %%
-model = LitResnet(lr=0.05)
-model.datamodule = cifar10_dm
-
-trainer = Trainer(
+trainer = pl.Trainer(
     progress_bar_refresh_rate=10,
-    max_epochs=30,
-    gpus=AVAIL_GPUS,
-    logger=TensorBoardLogger("lightning_logs/", name="resnet"),
+    max_epochs=cfg.num_epochs,
+    gpus=1,
+    logger=CSVLogger("lightning_logs/", name="resnet"),
     callbacks=[LearningRateMonitor(logging_interval="step")],
 )
 
 trainer.fit(model, cifar10_dm)
+
+# %%
 trainer.test(model, datamodule=cifar10_dm)
 # %%
