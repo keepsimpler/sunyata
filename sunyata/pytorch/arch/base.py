c8ce6b59d1ee8cd66b1105495e896a7e04e615e2
feng wenfeng <fengwenfeng@gmail.com>
2022-05-31 16:28:10 +0800

    Basic Config and Module for pl training

--- sunyata/pytorch/arch/base.py
+++ sunyata/pytorch/arch/base.py
@@ -1,4 +1,7 @@
+from lib2to3.pytree import Base
+import torch
 from dataclasses import dataclass
+import pytorch_lightning as pl
 
 
 @dataclass
@@ -12,3 +15,34 @@ class BaseCfg:
     weight_decay: float = None
     optimizer_method: str = "Adam"
     learning_rate_scheduler: str = "CosineAnnealing"
+
+
+class BaseModule(pl.LightningModule):
+    def __init__(self, cfg:BaseCfg):
+        super().__init__()
+        self.cfg = cfg
+
+    def training_step(self, batch, batch_idx):
+        return self._step(batch, mode="train")
+
+    def validation_step(self, batch, batch_idx):
+        return self._step(batch, mode="val")
+
+    def configure_optimizers(self):
+        if self.cfg.optimizer_method == "Adam":
+            optimizer = torch.optim.Adam(self.parameters(), lr=self.cfg.learning_rate)
+        elif self.cfg.optimizer_method == "AdamW":
+            optimizer = torch.optim.AdamW(self.parameters(), lr=self.cfg.learning_rate, weight_decay=self.cfg.weight_decay)
+        else:
+            raise Exception("Only support Adam and AdamW optimizer till now.")
+
+        if self.cfg.learning_rate_scheduler == "CosineAnnealing":
+            lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, self.cfg.num_epochs)
+        else:
+            lr_scheduler = None
+
+        if lr_scheduler is None:
+            return optimizer
+        else:
+            return [optimizer], [lr_scheduler]   
+
