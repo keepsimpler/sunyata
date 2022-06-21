73230f4976a725e8bc7ec89b3332fcf827bb6aee
feng wenfeng <fengwenfeng@gmail.com>
2022-06-21 17:16:51 +0800

    add batchnorm as predictor

---
+++ notebooks/byol_clm_wikitext2.py
@@ -0,0 +1,86 @@
+# %%
+from dataclasses import dataclass
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import pytorch_lightning as pl
+from sunyata.pytorch.arch.base import BaseCfg, BaseModule
+
+# %%
+from sunyata.pytorch.data.wikitext import (WikiTextDataModule,
+                                            shift_one_token)
+
+from sunyata.pytorch.layer.transformer import TransformerCfg, TransformerLayer
+
+from sunyata.pytorch.arch.byol_clm import BYOL_CLM, BYOL_CLM_Cfg, BYOL_EMA
+
+# %%
+hidden_dim = 64
+cfg = BYOL_CLM_Cfg(
+    vocab_size = 10000,
+    seq_len = 128,
+    hidden_dim = hidden_dim,
+    ema_tau = 0.999,  # 0.9999
+    transformer = TransformerCfg(
+        hidden_dim = hidden_dim,
+        num_heads = 2,
+        expanded_dim= 2*hidden_dim,
+        is_softmax=True,
+    ),
+
+    batch_size = 64,
+    num_layers = 4,
+    num_epochs = 1,
+    learning_rate = 1e-3 # 1e-3  3e-4
+)
+
+# %%
+wikitext2 = WikiTextDataModule(subset="2", 
+                   data_dir=".data/wikitext/", 
+                   batch_size=cfg.batch_size,
+                   vocab_size=cfg.vocab_size,
+                   seq_len=cfg.seq_len,
+                   collate_fn=shift_one_token)  # shift_one_token  None
+# %%
+wikitext2.tokenizer.decode(wikitext2.train_data[0].tolist(), skip_special_tokens=False)
+# https://colab.research.google.com/github/huggingface/notebooks/blob/master/examples/tokenizer_training.ipynb
+# https://www.thepythoncode.com/article/pretraining-bert-huggingface-transformers-in-python
+
+# %%
+input, target = next(iter(wikitext2.train_dataloader()))
+input.shape, target.shape
+
+# %%
+byol_clm = BYOL_CLM(cfg)
+byol_clm.summarize(max_depth=2)
+# %%
+csv_logger = pl.loggers.CSVLogger(save_dir="lightning_logs/", 
+    name="wikitext_2") # , version=2
+trainer = pl.Trainer(gpus=1, 
+                     max_epochs=cfg.num_epochs, 
+                     enable_checkpointing=False,
+                     callbacks=[BYOL_EMA(cfg.ema_tau)],
+                    #  limit_train_batches=100,  # 1.0 
+                    #  limit_val_batches=10,  # 1.0 
+                     log_every_n_steps=50,
+                     logger=csv_logger)
+
+# %%
+trainer.fit(byol_clm, wikitext2)
+
+# %%
+# for i, (input, target) in enumerate(wikitext2.train_dataloader()):
+online_pred, target_proj = byol_clm(input, target)
+online_pred.shape, target_proj.shape
+# %%
+torch.std(target_proj, dim=1), torch.mean(target_proj, dim=1)
+# %%
+torch.std(online_pred, dim=1), torch.mean(online_pred, dim=1)
+# %%
+byol_clm.loss_fn(online_pred, target_proj).mean()
+# %%
+2 - 2 * nn.CosineSimilarity(dim=-1)(online_pred, target_proj)
+# %%
+byol_clm.loss_fn(online_pred, target_proj)
+# %%
--- sunyata/pytorch/arch/byol_clm.py
+++ sunyata/pytorch/arch/byol_clm.py
@@ -33,24 +33,6 @@ def across_loss_fn(x: torch.Tensor, y: torch.Tensor):
     loss = 2 + min_loss.mean() - max_loss.mean()    
 
 
-# exponential moving average
-
-class EMA():
-    def __init__(self, beta):
-        super().__init__()
-        self.beta = beta
-
-    def update_average(self, old, new):
-        if old is None:
-            return new
-        return old * self.beta + (1 -  self.beta) * new
-
-def update_moving_average(ema_updater, ma_model, current_model):
-    for current_params, ma_params in zip(current_model.parameters(), ma_model.parameters()):
-        old_weight, up_weight = ma_params.data, current_params.data
-        ma_params.data = ema_updater.update_average(old_weight, up_weight)
-
-
 @dataclass
 class BYOL_CLM_Cfg(BaseCfg):
     vocab_size: int = None
@@ -68,49 +50,74 @@ class BYOL_CLM(BaseModule):
         super().__init__(cfg)
         self.save_hyperparameters('cfg')
 
+        self.embed = nn.Embedding(cfg.vocab_size, cfg.hidden_dim)
+
         self.online_encoder = nn.Sequential(
-            nn.Embedding(cfg.vocab_size, cfg.hidden_dim),
+            # nn.Embedding(cfg.vocab_size, cfg.hidden_dim),
             nn.Sequential(*[
                 TransformerLayer(cfg.transformer) for _ in range(cfg.num_layers)
-            ])
+            ]),
+            BatchNorm(cfg.hidden_dim)
         )
-        torch.nn.init.xavier_normal_(self.online_encoder[0].weight.data)
+        torch.nn.init.xavier_normal_(self.embed.weight.data)  # online_encoder[0]
 
         self.target_encoder = copy.deepcopy(self.online_encoder)
         # self.target_encoder.eval()
         set_requires_grad(self.target_encoder, False)
 
-        for layer in self.target_encoder[1]:
+        for layer in self.target_encoder[0]:
             layer.attention.is_mask = False
 
-        self.online_predictor = TransformerLayer(cfg.transformer)
+        # self.online_predictor = TransformerLayer(cfg.transformer)
+        self.online_predictor = BatchNorm(cfg.hidden_dim)
 
         self.loss_fn = loss_fn
         self.cfg = cfg
 
     def forward(self, input, target):
+        input = self.embed(input)
         online_proj = self.online_encoder(input)
         online_pred = self.online_predictor(online_proj)
 
         with torch.no_grad():
+            target = self.embed(target)
             target_proj = self.target_encoder(target)
             target_proj.detach_()
 
-        loss = self.loss_fn(online_pred, target_proj)
-        return loss.mean()
+        return online_pred, target_proj
 
     def _step(self, batch, mode="train"):  # or "val"
         input, target = batch
-        loss = self.forward(input, target)
+        online_pred, target_proj = self.forward(input, target)
+        loss = self.loss_fn(online_pred, target_proj).mean()
         self.log(mode + "_loss", loss)
         return loss
 
-    def training_step(self, batch, batch_idx):
-        return self._step(batch, mode="train")
 
-    def validation_step(self, batch, batch_idx):
-        return self._step(batch, mode="val")
-   
+class BatchNorm(nn.Module):
+    def __init__(self, hidden_dim, mom=0.1, eps=1e-5):
+        super().__init__()
+        self.mom, self.eps = mom, eps
+        self.mults = nn.Parameter(torch.ones(1, hidden_dim))
+        self.adds = nn.Parameter(torch.zeros(1, hidden_dim))
+        self.register_buffer('vars', torch.ones(1, 1, hidden_dim))
+        self.register_buffer('means', torch.zeros(1, 1, hidden_dim))
+
+    def update_stats(self, x):
+        # x has dims (batch_size, seq_len, hidden_dim)
+        m = x.mean((0,1), keepdim=True)
+        v = x.var((0,1), keepdim=True)
+        self.means.lerp_(m, self.mom)
+        self.vars.lerp_(v, self.mom)
+        return m, v
+
+    def forward(self, x):
+        if self.training:
+            with torch.no_grad(): m, v = self.update_stats(x)
+        else: m,v = self.means, self.vars
+        x = (x-m) / (v+self.eps).sqrt()
+        return x * self.mults + self.adds
+
 
 class BYOL_EMA(Callback):
     def __init__(self, initial_tau: float=0.999, do_tau_update: bool=True):
