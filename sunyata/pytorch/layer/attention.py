b17b2219a081dcbdc583efd30af50cd6b6826255
feng wenfeng <fengwenfeng@gmail.com>
2022-06-28 07:10:12 +0800

    modify non-softmax

--- sunyata/pytorch/layer/attention.py
+++ sunyata/pytorch/layer/attention.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from einops import rearrange
 
 
@@ -15,7 +16,7 @@ class Attention(nn.Module):
 
         self.to_qkv = nn.Linear(hidden_dim, hidden_dim * 3)
         
-        self.attend = nn.Softmax(dim=-1) if is_softmax else nn.Identity()
+        self.softmax = nn.Softmax(dim=-1) # if is_softmax else nn.Identity()
 
         self.to_out = nn.Sequential(
             nn.Linear(hidden_dim, hidden_dim),
@@ -31,17 +32,21 @@ class Attention(nn.Module):
         
         q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=self.num_heads), (q, k, v))
 
+        # q = F.normalize(q, dim=-1, p=2)
+        # k = F.normalize(k, dim=-1, p=2)
         dots = torch.einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
         if self.is_mask and self.is_softmax:
             attn_mask = torch.full((seq_len, seq_len), -float('Inf'), device=x.device, dtype=x.dtype)
             attn_mask = torch.triu(attn_mask, diagonal=1)
             dots = dots + attn_mask
+            dots = self.softmax(dots)
         elif self.is_mask and not self.is_softmax:
             attn_mask = torch.ones((seq_len, seq_len), device=x.device, dtype=x.dtype)
             attn_mask = torch.tril(attn_mask)
             dots = dots * attn_mask
+            dots = F.normalize(dots, dim=-1, p=2)
 
-        attn = self.attend(dots)
+        attn = dots
         
         out = torch.einsum('b h i j, b h j d -> b h i d', attn, v)
         out = rearrange(out, 'b h n d -> b n (h d)')
