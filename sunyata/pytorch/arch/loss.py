2b8521044150c694139201b2b99fbcd0b94b726b
feng wenfeng <fengwenfeng@gmail.com>
2022-07-25 08:16:15 +0800

    infonce along batch_size

--- sunyata/pytorch/arch/loss.py
+++ sunyata/pytorch/arch/loss.py
@@ -2,6 +2,24 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+def infoNCE2(z1:torch.Tensor, z2:torch.Tensor, temperature:float=1.):
+    batch_size, seq_len, hidden_dim = z1.shape
+    logits = torch.einsum('a n, b n -> a b', z1.reshape(batch_size,-1), z2.reshape(batch_size,-1))
+    logits /= temperature
+    batch_size, seq_len, hidden_dim = z1.shape
+    labels = torch.arange(0, batch_size, dtype=torch.long, device=z1.device)
+    loss = F.cross_entropy(logits, labels)
+    return loss
+
+class InfoNCE2(nn.Module):
+    def __init__(self, temperature:float = 1.):
+        super().__init__()
+        self.temperature = temperature
+
+    def forward(self, z1:torch.Tensor, z2:torch.Tensor):
+        return infoNCE2(z1, z2, self.temperature)
+
+
 def infoNCE(z1:torch.Tensor, z2:torch.Tensor, temperature:float=1.):
     logits = torch.einsum('b s n, b t n -> b s t', z1, z2)
     logits /= temperature
@@ -35,6 +53,59 @@ class BarlowTwins(nn.Module):
 
     def forward(self, z1:torch.Tensor, z2:torch.Tensor):
         return barlow_twins(z1, z2, self.temperature)
+
+
+class BarlowTwinsLoss2d(nn.Module):
+    def __init__(self, batch_size, lambda_coeff=5e-3, z_dim=128):
+        super().__init__()
+
+        self.z_dim = z_dim
+        self.batch_size = batch_size
+        self.lambda_coeff = lambda_coeff
+
+    def off_diagonal_ele(self, x:torch.Tensor):
+        n, m = x.shape
+        assert n == m
+        return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
+
+    def forward(self, z1, z2):
+        # N x D, where N is the batch_size and D is output dim of projection head
+        z1_norm = (z1 - torch.mean(z1, dim=0)) / torch.std(z1, dim=0)
+        z2_norm = (z2 - torch.mean(z2, dim=0)) / torch.std(z2, dim=0)
+
+        cross_corr = torch.matmul(z1_norm.T, z2_norm) / self.batch_size
+
+        on_diag = torch.diagonal(cross_corr).add_(-1).pow_(2).sum()
+        off_diag = self.off_diagonal_ele(cross_corr).pow_(2).sum()
+
+        loss = on_diag + self.lambda_coeff * off_diag
+        return loss
+
+
+class BarlowTwinsLoss3d(nn.Module):
+    def __init__(self, lambda_coeff=5e-3):
+        super().__init__()
+
+        # self.seq_len = seq_len
+        self.lambda_coeff = lambda_coeff
+
+    def off_diagonal_ele(self, x:torch.Tensor):
+        b, n, m = x.shape
+        assert n == m
+        return x.flatten(start_dim=1)[:,:-1].view(-1, n-1, n+1)[:,:,1:].flatten(start_dim=1)
+
+    def forward(self, z1, z2):
+        # N x S x D, where N is the batch_size, S is the sequence length and D is output dim of projection head
+        z1_norm = (z1 - torch.mean(z1, dim=(0,1))) / torch.std(z1, dim=(0,1))
+        z2_norm = (z2 - torch.mean(z2, dim=(0,1))) / torch.std(z2, dim=(0,1))
+
+        cross_corr = torch.einsum('b s n, b s m -> b n m', z1_norm, z2_norm)
+
+        on_diag = torch.diagonal(cross_corr, dim1=1, dim2=2).add_(-1).pow_(2).sum(-1).mean()
+        off_diag = self.off_diagonal_ele(cross_corr).pow_(2).sum(-1).mean()
+
+        loss = on_diag + self.lambda_coeff * off_diag
+        return loss
 
 
 # copy from https://github.com/gpleiss/temperature_scaling/blob/126a50975e/temperature_scaling.py
