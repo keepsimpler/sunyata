9ef3e6108b4160096549b8e0d44dbc217eef69c0
feng wenfeng <fengwenfeng@gmail.com>
2023-03-06 00:43:02 +0800

    remove norm layer, without centering and bias

--- sunyata/pytorch/arch/deit.py
+++ sunyata/pytorch/arch/deit.py
@@ -242,7 +242,7 @@ class bayes_vit_models(vit_models):
 
         log_prior = torch.zeros(1, num_classes)
         self.register_buffer('log_prior', log_prior)
-        self.logits_bias = nn.Parameter(torch.zeros(1, num_classes))
+#         self.logits_bias = nn.Parameter(torch.zeros(1, num_classes))
         # self.logits_layer_norm = nn.LayerNorm(num_classes)
         self.norm = None
 
@@ -263,7 +263,7 @@ class bayes_vit_models(vit_models):
             logits = x[:, 0]
             logits = self.head(logits)
             log_prior = log_prior + logits
-            log_prior = log_prior - torch.mean(log_prior, dim=-1, keepdim=True) + self.logits_bias
+#             log_prior = log_prior - torch.mean(log_prior, dim=-1, keepdim=True) + self.logits_bias
             log_prior = F.log_softmax(log_prior, dim=-1)
             # log_prior = self.logits_layer_norm(log_prior)
         
